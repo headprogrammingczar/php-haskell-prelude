499cd773e130f964f29547dfa4090d42c35367b6
databox <headprogrammingczar@gmail.com>
2012-05-04 12:03:23 -0400

    num instances

--- PHP.hs
+++ PHP.hs
@@ -16,6 +16,48 @@ instance Num String where
 
   signum x = if abs x == x then 1 else -1
 
+instance Real String where
+  toRational = toRational . read
+
+instance Enum String where
+  toEnum = show
+  fromEnum = read
+
+instance Integral String where
+  quot x y = show (read x `quot` read y)
+  rem x y = show (read x `rem` read y)
+  div x y = show (read x `div` read y)
+  mod x y = show (read x `mod` read y)
+
+  toInteger = read
+
+instance Fractional String where
+  (/) x y = show (read x / read y)
+  recip x = show (recip (read x))
+  fromRational = show
+
+instance Floating String where
+  pi = "3.14"
+  exp x = show (exp (read x))
+  sqrt x = show (sqrt (read x))
+  log x = show (log (read x))
+
+  sin x = show (sin (read x))
+  cos x = sin (x + 90)
+  sinh x = show (sinh (read x))
+  cosh x = sinh (x + 90)
+  asin x = show (asin (read x))
+  acos x = asin (x + 90)
+  atan x = asin x / acos x
+  asinh x = show (asinh (read x))
+  acosh x = asinh (x + 90)
+  atanh x = asinh x / acosh x
+
+instance RealFrac String where
+  properFraction x = (proper, frac) where proper = fromInteger (read (takeWhile (/= '.') x)); frac = tail (dropWhile (/= '.') x)
+
+-- TODO - RealFloat
+
 sort = sortBy compare
 
 -- sort function, optimized for lists
